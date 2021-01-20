6b762914f7ad30149689892d4908f145ba4a8c20
Prana Tantri <67408817+ProgrammingWithPranav@users.noreply.github.com>
2021-01-20 21:17:59 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
+function send(){
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_ans = parseInt(number1) * parseInt(number2);
 
+    question_number = "<h4>" + number1 + " X " + number2 + "</h4>";
+    input_box = "<br>Answer : <input type='text' id='check' />";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row = question_number + input_box + check_button;
+
+    document.getElementById("questionOutput").innerHTML = row;
+
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
+}
