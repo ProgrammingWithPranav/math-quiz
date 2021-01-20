7336ced903f607ea333e9030e2209894b9aa3fe8
Prana Tantri <67408817+ProgrammingWithPranav@users.noreply.github.com>
2021-01-20 21:10:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function saveUsername(){
+    player1Name = document.getElementById("player1_name").value;
+    player2Name = document.getElementById("player2_name").value;
+
+    localStorage.setItem("player1Name", player1Name);
+    localStorage.setItem("player2Name", player2Name);
+
+    window.location = 'game_page.html';
+}
